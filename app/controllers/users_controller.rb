ddad8f0c9b43e4716b4fc1f41acb717b4709bcd6
DIANA JURGIELEWICZ <DMJURG@GMAIL.COM>
2015-08-05 22:36:06 -0400

    fixed routes for favoriting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
+
     @users = User.alphabetical
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   resources :reviews
   resources :wineries do
-    get 'favorite', on: :member
+    post 'favorite', on: :member
     post 'unfavorite', on: :member
   end
   resources :users
